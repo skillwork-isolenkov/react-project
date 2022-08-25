244b89b5a0829c6af6e66a18bf54f8ec18ac24dd
skillwork-isolenkov <ivan_solenkov1999@abv.bg>
2022-08-25 16:21:32 +0300

    Sass installed + first component example

---
+++ src/Main/Components/Random/Random.jsx
@@ -0,0 +1,11 @@
+import './random.scss';
+
+const Random = () => {
+  return (
+      <div className="random">
+        <h1 className="random__header">Random!</h1>
+      </div>
+  )
+}
+
+export default Random;
