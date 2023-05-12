d49842d3c8e868bc55b6414ae052af5425c69c4c
skillwork-isolenkov <ivan_solenkov1999@abv.bg>
2023-05-12 19:28:25 +0300

    RP-01 React-Bootstrap installed + custom theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Random from "./Main/Components/Random/Random";
 
+import "./Styles/App.scss";
+
 function App() {
   return (
     <div className="App">
--- src/Main/Components/Random/Random.jsx
+++ src/Main/Components/Random/Random.jsx
@@ -1,9 +1,21 @@
 import './random.scss';
+import { Button } from 'react-bootstrap';
 
 const Random = () => {
   return (
       <div className="random">
         <h1 className="random__header">Random!</h1>
+        <div className="random__buttons-wrapper">
+          <Button variant="primary">Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="success">Success</Button>
+          <Button variant="warning">Warning</Button>
+          <Button variant="danger">Danger</Button>
+          <Button variant="info">Info</Button>
+          <Button variant="light">Light</Button>
+          <Button variant="dark">Dark</Button>
+          <Button variant="link">Link</Button>
+        </div>
       </div>
   )
 }
