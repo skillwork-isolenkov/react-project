02d57fa2dc3c6760e3a93b9087eaea0e2a9d672e
skillwork-isolenkov <ivan_solenkov1999@abv.bg>
2023-05-12 20:21:30 +0300

    RP-03 Added Second component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Random from "./Main/Components/Random/Random";
+import Second from "./Main/Components/Second/Second";
 
 import "./Styles/App.scss";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Random />
+      <Second />
     </div>
   );
 }
---
+++ src/Main/Components/Second/Second.jsx
@@ -0,0 +1,11 @@
+import './second.scss';
+
+const Second = () => {
+  return (
+      <div className="second">
+        Adadasdsa
+      </div>
+  )
+}
+
+export default Second;
